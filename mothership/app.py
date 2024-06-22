703a5531b44b6b6a100cf002ae641e43060e54a0
Gyanendra Mishra <anomaly.the@gmail.com>
2024-06-22 17:30:45 +0100

    bigger field

--- mothership/app.py
+++ mothership/app.py
@@ -27,7 +27,7 @@ jwt = JWTManager(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
 
 
 class Bot(db.Model):
